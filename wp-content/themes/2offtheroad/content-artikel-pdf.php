fc3a7cb1247a28043609b9ada9de6daf9c963242
Tim Muller <t_muller@hotmail.com>
2013-04-20 11:25:38 +0200

    different view added for pdf

---
+++ wp-content/themes/2offtheroad/content-artikel-pdf.php
@@ -0,0 +1,48 @@
+<?php
+$args = 
+?>
+
+
+
+<!--
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <h3>
+    <?php the_title(); ?>
+  </h3>
+
+<?php
+       $total_content = "";
+        while ( have_posts() ) : the_post();
+          ob_start();
+          the_content();
+          $total_content = ob_get_contents();
+          ob_end_clean();
+        endwhile;
+        $total_content = strip_shortcodes($total_content);
+        $totallength = strlen($total_content);
+        $end_of_last_p_for_left = strpos($total_content, "\n", ($totallength / 2));
+        $left_column_content = substr($total_content, 0, $end_of_last_p_for_left);
+        $right_column_content = substr($total_content, $end_of_last_p_for_left, $totallength);
+
+
+?>
+
+
+  <div class="columnleft">
+    <div class="columnspacing">
+    <?php 
+        echo $left_column_content;
+    ?>
+    </div>
+  </div>
+  <div class="columnright">
+    <div class="columnspacing">
+    <?php get_template_part( 'content-image' ); ?>
+    <?php 
+        echo $right_column_content;
+    ?>
+    </div>
+  </div>
+  <div class="clearfix"></div>
+</article>
+i-->
--- wp-content/themes/2offtheroad/content.php
+++ wp-content/themes/2offtheroad/content.php
@@ -1,8 +1,12 @@
 <?php
       $my_type = types_render_field('my_type', array('raw' => 'true'));
-      if($my_type = "artikel"){
+      if($my_type == "artikel"){
       	get_template_part('content-artikel');
-      }  else {
+      }
+      elseif ($my_type = 'artikel-pdf'){
+      	get_template_part('content-artikel-pdf');
+      } 
+      else {
       	echo "Please define a type for the page";
       }
 ?>
