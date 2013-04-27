62f8a861746cc9610be26498ba9099faa41c868c
Tim Muller <tim@tim-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-04-27 13:08:01 +0200

    overview of blogs in category with thumbnail

--- wp-content/themes/2offtheroad/category.php
+++ wp-content/themes/2offtheroad/category.php
@@ -1,4 +1,69 @@
 <?php
 get_header();
+?>
+
+<h1>
+  Blog van : <?php  echo single_cat_title( '', false )?>
+</h1>
+<div class="center">
+<?php
+  $column  = 4;
+  $stories = array();
+  while ( have_posts() ) : the_post();
+    $stories[] = array('id' => get_the_ID(), 'title' => get_the_title(), 'date' => get_the_date(), 'url' => get_permalink());
+  endwhile;
+
+  $totalsize = sizeof($stories);
+  $total_rows = ceil($totalsize / $column);
+
+  for($row = 0; $row < $total_rows; $row++){
+?>
+  <div class="thumbnailrow">
+<?php
+    for($current_column = 1; $current_column <= $column; $current_column++){
+      $current_element = $stories[($row * $column) + ($current_column-1)];
+      if(!$current_element){
+	break;
+      }
+      
+      $fetch_attachments = array( 'post_type' => 'attachment', 'post_parent' => $current_element['id'] );
+      $attachments = get_posts($fetch_attachments);
+      if ($attachments) {
+        foreach ( $attachments as $attachment ) {
+	  $current_element['attach'] = $attachment;
+	}
+      }
+	
+?>
+  <div class="storywrapper" style="display:inline-block">
+  <a href="<?php echo $current_element['url'] ?>">
+  <div class="thumbnail" style="background-image:URL( 
+  <?php
+	if(!$current_element['attach']){
+  ?>
+      <?php echo get_template_directory_uri(); ?>/img/unknown_blog.jpg
+  <?php
+ 	} else {
+  ?>
+      <?php echo $current_element['attach']->guid ?>
+  <?php
+	}
+  ?>
+    );"></div>
+    <div class="title"><?php echo $current_element['title'] ?></div>
+    <div class="date"><?php echo $current_element['date'] ?></div>
+  </a>
+  </div>
+<?php
+    }
+?>
+  </div>
+  <div class="clearfix"></div>
+<?php
+  }
+
+?>
+</div>
+<?php
 get_footer();
 ?>
--- wp-content/themes/2offtheroad/content.php
+++ wp-content/themes/2offtheroad/content.php
@@ -1,9 +1,4 @@
 <?php
       $my_type = types_render_field('my_type', array('raw' => 'true'));
-      if($my_type == "artikel"){
-      	get_template_part('content-artikel');
-      }
-      else {
-      	echo "Please define a type for the page";
-      }
+      get_template_part('content-artikel');
 ?>
