05152d5f2ccd20bb6140d677bb093063ed2ef6d8
Tim Muller <tim@tim-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-04-27 14:34:21 +0200

    added plugin for live editting

--- wp-content/themes/2offtheroad/header.php
+++ wp-content/themes/2offtheroad/header.php
@@ -1,5 +1,6 @@
 <html>
   <head>
+    <?php wp_head(); ?>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width" />
     <title><?php bloginfo('name') ?></title>
