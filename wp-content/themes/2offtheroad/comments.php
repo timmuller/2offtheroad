d3d471686b5954bb9f21452cf7d29bbae1a798f0
Tim Muller <tim@tim-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-04-30 18:43:21 +0200

    no comments yet

--- wp-content/themes/2offtheroad/comments.php
+++ wp-content/themes/2offtheroad/comments.php
@@ -1,5 +1,10 @@
 <br/>
 <h2>Reacties</h2>
+<?php
+  if(! have_comments() ){
+	print "Er zijn nog geen reacties geplaatst.<br/>Je kunt de eerste zijn die reageert op deze post!";
+  }
+ ?>
 <ul class="commentlist">
 	<?php wp_list_comments('type=comment&callback=custom_comments'); ?>
 <!--	<?php wp_list_comments(); ?> -->
