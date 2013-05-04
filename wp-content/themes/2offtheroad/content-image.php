7162bf37b68b06d6e673f86a04b62a130238802d
Tim Muller <tim@tim-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-05-04 14:46:26 +0200

    ie image fix

--- wp-content/themes/2offtheroad/content-image.php
+++ wp-content/themes/2offtheroad/content-image.php
@@ -4,23 +4,35 @@ $attachments = get_posts($args);
 if ($attachments) {
 	foreach ( $attachments as $attachment ) {
 ?>
+
+<?php
+    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $ub = '';
+    if(preg_match('/MSIE/i',$u_agent))
+    {
+	$ub = 'ie';
+    }
+?>
+
 <div class="image_wrapper">
+<?php if ($ub == 'ie') { ?>
+  <a href="<?php echo $attachment->guid ?>">
+<?php  } else { ?>
   <a href="#fullImage" role="button" data-toggle="modal">
+<?php } ?>
     <img src="<?php echo $attachment->guid ?>" />
   </a>
   <div class="image_wrapper_subtitle"><?php echo $attachment->post_excerpt ?></div>
 </div>
 
-
 <div id="fullImage" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-<button class="close" data-dismiss="modal" data-target="#myModal">×</button>
-<div class="modal-body" data-dismiss="modal" data-target="#myModal">
-    <img src="<?php echo $attachment->guid ?>" />
-</div>
+  <button class="close" data-dismiss="modal" data-target="#myModal">×</button>
+  <div class="modal-body" data-dismiss="modal" data-target="#myModal">
+      <img src="<?php echo $attachment->guid ?>" />
+  </div>
 </div>
 
 <?php
-
 	}
 }
 ?>
